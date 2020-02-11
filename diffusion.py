0c6f080a05ec4e142b05b31e42669fe01554db82
Angelo Charry <38404747+TheLordOfLambs@users.noreply.github.com>
2020-02-11 14:43:12 +0100

    correcting main script

--- diffusion.py
+++ diffusion.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 19:12:52 2020
 
+@author: Angelo Charry
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from scipy.signal import convolve2d
-import time
+
 
 def fill(B,centre,radius) :
+    """
+    This fonction add a chemical with a circular distribution
+    """
     
     for x in range(B.shape[0]) :
         for y in range(B.shape[1]) :
@@ -19,6 +26,9 @@ def fill(B,centre,radius) :
     return B
 
 def remove(B,centre,radius) :
+    """
+    This fonction remove a chemical with a circular distribution
+    """
     
     for x in range(B.shape[0]) :
         for y in range(B.shape[1]) :
@@ -32,33 +42,37 @@ def remove(B,centre,radius) :
 
 plt.close('all')
 
-h = 500#480
-H = 1500#720
+h = 480                            # hauteur
+l = 720                            # largeur
 
 D_A = 1                            # diffusion rate chimical A
 D_B = 0.5                           # diffusion rate chimical B
 f = 0.0545                           # feeding rate chimical A
 k = 0.062                           # killing rate chimical B
 dt = 1                               # time steps in sec
-maxiter = 5000                        # iterations nubers
-screen_rate = 100
+maxiter = 6000                        # iterations nubers
+screen_rate = 30                      # rate of images saving
+image_path = "Images/"              # where the images will be saved
 
-'''
-A = np.ones((h,H))
-B = np.zeros((h,H))
 
-B = fill(B,(250,750),20)
-'''
+
+
+# initializing the grid
+A = np.ones((h,l))
+B = np.zeros((h,l))
+
+B = fill(B,(240,360),10) # filling the grid with chemical B
+
 
 laplace_filter = filt = np.array([[0.05,0.2,0.05],
                                   [0.2,-1,0.2],
                                   [0.05,0.2,0.05]])
-'''
+
 plt.figure(1)
 plt.imshow(B)
 plt.title("t = 0 sec")
 plt.show()
-'''
+
 j=0
 for i in tqdm(range(maxiter)) :
     
@@ -67,25 +81,9 @@ for i in tqdm(range(maxiter)) :
     
     A = A + dt*(D_A*lapA-A*B*B+f*(1-A))
     B = B + dt*(D_B*lapB+A*B*B-(k+f)*B)
-    '''
-    if i%50 == 0 :
-        
-        plt.imsave("{}.png".format(str(j).zfill(5)),B)
+    
+    if i%screen_rate == 0 :
+        plt.imsave(image_path+"{}.png".format(str(j).zfill(5)),B) #saving the image
         j += 1
         
-    '''
-'''
-    if  i%screen_rate == 0 :
-        time.sleep(0.1)
-        plt.figure(1)
-        plt.title("t = {} sec".format(i*dt))
-        plt.imshow(B)
-        plt.show()
-        plt.pause(0.1)
-
-'''
-plt.figure(1)
-plt.imshow(B,cmap='jet')
-plt.title("t = {} sec".format(maxiter*dt))
-plt.show()
 
